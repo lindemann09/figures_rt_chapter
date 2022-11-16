66996912b0817510bc5d9f086e34a9b5d44e69ff
Oliver Lindemann <lindemann09@googlemail.com>
2022-11-16 09:52:07 +0100

    Posner figure

---
+++ Posner.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+
+df = tibble(fa= factor(c("yes", "no")),
+            fb1=c(334, 78),
+            fb2=c(89, 34))
+
+dat =pivot_longer(df, cols = c("fb1", "fb2"), names_to = "fb")
+ggplot(data=dat,
+       aes(x=fa, y=value, group=fb))  +
+       geom_point(size = 4, aes(colour=fb, shape=fb, fill=fb))+
+       geom_line(size=1, aes(colour=fb)) +
+       ylab("Mean RT (ms)") +
+       xlab("X label") +
+       theme_bw(base_size=18)
