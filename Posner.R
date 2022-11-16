cf2bfc26c74225a2d8953b4f9c80df337ad2e08f
Oliver Lindemann <lindemann09@googlemail.com>
2022-11-16 12:09:16 +0100

    update

--- Posner.R
+++ Posner.R
@@ -1,21 +1,42 @@
-library(tidyr)
+library(tidyverse)
 library(ggplot2)
 
+# make date
+br_effect = 20
+ro_effect = 50
+m = 506
+df = tibble(ro= factor(c("colour", "suit")),
+            bright=c(m, m+ro_effect),
+            blurred=c(m+br_effect, m+br_effect+ro_effect))
 
-df = tibble(fa= factor(c("yes", "no")),
-            fb1=c(334, 78),
-            fb2=c(89, 34))
+data = df %>% pivot_longer(cols = c("bright", "blurred"),
+                    names_to = "Brightness") %>%
+        mutate(cond="a) Same Processing Stage")
 
+# add interaction
+tmp = data %>%
+  mutate(value = value + c(0,0, -25, 20),
+         cond="b) Different Processing Stage")
+data = rbind(data, tmp) %>% mutate(
+  Brightness =factor(Brightness)
+)
 
-dat =pivot_longer(df, cols = c("fb1", "fb2"), names_to = "fb")
+# plot
+plt = ggplot(data=data,
+       aes(x=ro, y=value, group=Brightness))  +
+  geom_line(size=1, aes(colour=Brightness)) +
+  geom_point(color='white', stroke=3, shape=21, size=5, aes(fill=Brightness)) +
+  scale_fill_manual(values=c("#2299DD", "#66AA99")) +
+  scale_color_manual(values=c("#2299DD", "#66AA99")) +
+  ylab("Mean RT (ms)") +
+  xlab("Response Option") +
+  ylim(480, 620) +
+  facet_wrap(.~cond) +
+  theme_minimal(base_size=14) +
+  theme(axis.line = element_line(size=0.5),
+        axis.ticks = element_line(size=0.5))
 
-plt = ggplot(data=dat,
-       aes(x=fa, y=value, group=fb))  +
-       geom_point(size = 4, aes(colour=fb, shape=fb, fill=fb))+
-       geom_line(size=1, aes(colour=fb)) +
-       ylab("Mean RT (ms)") +
-       xlab("X label") +
-       theme_bw(base_size=18)
+print(plt)
 
-ggsave("Posner.png", plt, device="png")
+ggsave("Posner.png", plt, device="png", width=10, height = 4)
 
